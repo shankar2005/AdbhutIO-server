b5922961ce0872b22a02549930d935800e8ac5c6
shankar2005 <122206811+shankar2005@users.noreply.github.com>
2023-02-02 01:32:18 +0530

    add new link for test

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     # ==================== artist manager urls ===========================
     path('api/v1/artist_action/',ArtistActionviewSet.as_view(),name='artist_action'),
     path('api/v1/delete_project/<int:pk>/',ProjectDeleteViewSet.as_view(),name='delete_project'),
+    
+     # demo
+    path('api/v1/demo/',DemoView.as_view(),name="demo"),
 ]
