2a5c8a9b991babf4c23308ba170a1d926ae6d45d
shankar2005 <122206811+shankar2005@users.noreply.github.com>
2023-02-02 01:31:40 +0530

    update the signal

--- profiles/signals.py
+++ profiles/signals.py
@@ -10,7 +10,9 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 from django.dispatch import receiver
-from .models import Client,Project
+from .models import Client,Project,Role
+from rest_framework.authtoken.models import Token
+from django.shortcuts import get_object_or_404
 from django.core.files.storage import get_storage_class
 default_storage = get_storage_class()()
 
@@ -19,15 +21,24 @@ default_storage = get_storage_class()()
 def post_save_create_client(sender, instance, created, **kwargs):
 
     if created:
-        Client.objects.create(
-            user=instance,
-            name=instance.username,
-            email=instance.email,
-        )
+        if not Token.objects.filter(user = instance).exists():
+            Token.objects.create(user=instance)
+
+        if instance.username[:2] == 'AM':
+            Role.objects.create(user = instance,role="Artist Manager")
+        elif instance.username[:2] == 'PM':
+            Role.objects.create(user = instance,role="Product Manager")
+        else:
+            Role.objects.create(user = instance,role="Client")
+            Client.objects.create(
+                user=instance,
+                name=instance.username,
+                email=instance.email,
+            )
 
 @receiver(post_save,sender=Project)
 def post_save_update_project(sender,instance,created,**kwargs):
     if created:
         if instance.title is None:
-            instance.title = str(obj.project_template.name) + " - " + obj.stage + "-" + str(obj.id)
+            instance.title = str(instance.project_template.name) + " - " + instance.stage + " - " + str(instance.id)
             instance.save()
