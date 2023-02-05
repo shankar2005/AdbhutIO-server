b42564374fff02a0810417965660d13fbbe76a31
shankar2005 <122206811+shankar2005@users.noreply.github.com>
2023-02-05 11:43:32 +0530

    update the url

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     # ==================== artist manager urls ===========================
     path('api/v1/artist_action/',ArtistActionviewSet.as_view(),name='artist_action'),
     path('api/v1/delete_project/<int:pk>/',ProjectDeleteViewSet.as_view(),name='delete_project'),
-    
-     # demo
+    path('api/v1/update_title/<int:id>/',ProjectTitleViewSet.as_view(),name='update_title'),
+
+    # demo
     path('api/v1/demo/',DemoView.as_view(),name="demo"),
 ]
