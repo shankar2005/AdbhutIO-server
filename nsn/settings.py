d5f248f29d06a55d6d6ed8560a8df06b1c8f7c8b
Himanshu Garg <himan.gargcoder@gmail.com>
2023-04-26 11:46:52 +0530

    file upload to AWS S3

--- nsn/settings.py
+++ nsn/settings.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -188,6 +190,15 @@ SPECTACULAR_SETTINGS = {
 }
 
 
+AWS_ACCESS_KEY_ID = config("ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = config("SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = config("BUCKET_NAME")
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = config("REGION_NAME")
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' 
 # cloud storage
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # AWS_ACCESS_KEY_ID = "AKIARY2G35G2RQ5B45MI"
