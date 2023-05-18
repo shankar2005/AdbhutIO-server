727fdcd50f0b1fb249b520f5c60c5d1ab697e626
usama-py <mohd.usama984@gmail.com>
2023-05-18 22:30:47 +0530

    fixed error in artist post

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -529,7 +529,12 @@ class WorksLinkSerializer(serializers.ModelSerializer):
         ]
 
 class ArtistActionSerializer(serializers.ModelSerializer):
-    works_links = WorksLinkSerializer(many=True)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.method != 'PUT':
+            self.fields.pop('works_links')
+    works_links = WorksLinkSerializer(many=True, required=False)
     class Meta:
         model = Artist
         fields = [
