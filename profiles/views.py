9aa8aa7fd4ca4b60722bdb31d5c58473ec18584e
Nayan <nayan92001@gmail.com>
2023-12-16 22:33:31 +0530

    updated aws environ

--- profiles/views.py
+++ profiles/views.py
@@ -1556,9 +1556,9 @@ def extract_text(img_path):
     AWS_ACCESS_KEY_ID = config('ACCESS_KEY')
     AWS_SECRET_ACCESS_KEY = config('SECRET_ACCESS_KEY')
     AWS_DEFAULT_REGION = config('REGION_NAME')
-    os.environ["AWS_ACCESS_KEY_ID"] = AWS_ACCESS_KEY_ID
-    os.environ["AWS_SECRET_ACCESS_KEY"] = AWS_SECRET_ACCESS_KEY
-    os.environ["AWS_DEFAULT_REGION"] = AWS_DEFAULT_REGION
+    os.environ["ACCESS_KEY"] = AWS_ACCESS_KEY_ID
+    os.environ["SECRET_ACCESS_KEY"] = AWS_SECRET_ACCESS_KEY
+    os.environ["REGION_NAME"] = AWS_DEFAULT_REGION
     region_name = 'ap-south-1'
     rekognition = boto3.client('rekognition', region_name=region_name)
     with open(img_path, 'rb') as image_file:
